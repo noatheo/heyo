19822fc93aa1d62c0cdad2bee2cdd74fcb328bcb
noatheo <99247994+noatheo@users.noreply.github.com>
2022-02-21 18:03:36 +0200

    Update pyth.py

--- do/pyth.py
+++ do/pyth.py
@@ -1,1 +1,9 @@
-print("I am dangerous devops hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh engineer")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
